aa8625eef74166088b802486a96a52693fb70146
bethalaaa <67208911+bethalaaa@users.noreply.github.com>
2020-08-12 19:03:25 -0400

    Updated formatting

--- simple_chatty_bot.py
+++ simple_chatty_bot.py
@@ -2,12 +2,10 @@ def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
     print('I was created in ' + birth_year + '.')
 
-
 def remind_name():
     print('Please, remind me your name.')
     name = input()
     print('What a great name you have, ' + name + '!')
-
 
 def guess_age():
     print('Let me guess your age.')
@@ -20,7 +18,6 @@ def guess_age():
 
     print("Your age is " + str(age) + "; that's a good time to start programming!")
 
-
 def count():
     print('Now I will prove to you that I can count to any number you want.')
 
@@ -29,7 +26,6 @@ def count():
     while curr <= num:
         print(curr, '!')
         curr = curr + 1
-
 
 def test():
     def test():
@@ -48,10 +44,8 @@ def test():
     else:
         print("Completed, have a nice day!")
 
-
 def end():
     print('Congratulations, have a nice day!')
-
 
 greet('Aid', '2020')  # change it as you need
 remind_name()
